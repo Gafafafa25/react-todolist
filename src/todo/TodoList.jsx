2f4c33161935b04442eb4ee746c7bd18af5978d4
name <gafarova.tasia@gmail.com>
2026-02-09 19:15:34 +0300

    edit toggleTask

--- src/todo/TodoList.jsx
+++ src/todo/TodoList.jsx
@@ -1,15 +1,19 @@
 import TodoContext from "./TodoContext";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 
 
 const TodoList = () => {
     const {tasks, toggleTask} = useContext(TodoContext)
+
     return (
         <ul>
             {tasks.map(task => (
                 <li key={task.id}>
-                    <input type="checkbox" checked={task.done} onChange={() => toggleTask(task.id)}/>
-                    {task.text}
+                    <input type="checkbox" checked={task.done}
+                           onChange={() => toggleTask(task.id)}/>
+                        <span className={task.done ? "crossOutElement" : ""}>
+                            {task.text}
+                        </span>
                 </li>
             ))}
         </ul>
