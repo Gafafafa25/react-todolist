cf87f876536e54f874279fe42653ae681cdd5f28
name <gafarova.tasia@gmail.com>
2026-02-20 16:13:56 +0300

    add SERVER-PSQL 2

--- server-psql/server.js
+++ server-psql/server.js
@@ -1,20 +1,20 @@
 import express from 'express';
-// import {initDb} from "./db/init.js";
-import todosRouter from "./routes/todos.js"
+import {initDb} from "./db/init.js";
+// import todosRouter from "./routes/todos.js"
 
-// const app = express();
-// app.use(express.json());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// });
+const app = express();
+app.use(express.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 // app.use('/api', todosRouter)
-// // app.use(express.static('public'));
-//
-// initDb().then(() => {
-//     app.listen(3000, () => {
-//         console.log("Server running on port 3000")
-//     });
-// })
+// app.use(express.static('public'));
+
+initDb().then(() => {
+    app.listen(3000, () => {
+        console.log("Server running on port 3000")
+    });
+})
