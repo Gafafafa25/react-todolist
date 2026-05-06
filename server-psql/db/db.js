39a82ae2d1ea432be341a39b091223b3af46b40a
name <gafarova.tasia@gmail.com>
2026-05-06 12:45:22 +0300

    db.js changes

--- server-psql/db/db.js
+++ server-psql/db/db.js
@@ -1,15 +1,25 @@
 import pkg from 'pg';
+import {connectionString} from "pg/lib/defaults.js";
 
 const {Pool} = pkg;
+const connection = process.env.DATABASE_URL ?
+    {
+        connectionString: process.env.DATABASE_URL, ssl: {rejectUnauthorized: false}
+    }
+    :
+    {
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'myuser',
+        password: process.env.DB_PASSWORD || 'mypassword',
+        database: process.env.DB_NAME || 'reactTodolist'
+    }
 
-const pool = new Pool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'myuser',
-    password: process.env.DB_PASSWORD || 'mypassword',
-    database: process.env.DB_NAME || 'reactTodolist'
-});
+
+const pool = new Pool(connection);
 
 pool.on("connect", () => console.log("Successfully connected to db"));
-pool.on("connectionError", (err) => { console.error(err.message); });
+pool.on("connectionError", (err) => {
+    console.error(err.message);
+});
 
 export default pool;
